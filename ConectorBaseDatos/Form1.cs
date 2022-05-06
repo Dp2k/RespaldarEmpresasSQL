475afaf8fded3bd86af62fd77fbf4a7ec8fa7cb4
Dp2k <danielo_pacheco@hotmail.com>
2022-05-06 16:12:20 -0500

    Cambios6

--- ConectorBaseDatos/Form1.cs
+++ ConectorBaseDatos/Form1.cs
@@ -22,7 +22,6 @@ namespace ConectorBaseDatos{
             GetDataSources2();
         }
 
-   
         private void GetDataSources2(){
             string ServerName = Environment.MachineName;
             RegistryView registryView = Environment.Is64BitOperatingSystem ? RegistryView.Registry64 : RegistryView.Registry32;
@@ -48,122 +47,113 @@ namespace ConectorBaseDatos{
         private void button1_Click(object sender, EventArgs e){
             Microsoft.Office.Interop.Excel.Application xlApp = new Microsoft.Office.Interop.Excel.Application();
 
-            if (xlApp == null){
-                MessageBox.Show("Excel no esta instalado en la maquina, no se puede generar el reporte.");
-                return;
+            if (xlApp != null){
+                if (Directory.Exists(textBox3.Text)){
+                    Excel.Workbook xlWorkBook;
+                    Excel.Worksheet xlWorkSheet;
+                    object misValue = System.Reflection.Missing.Value;
 
+                    xlWorkBook = xlApp.Workbooks.Add(misValue);
+                    xlWorkSheet = (Excel.Worksheet)xlWorkBook.Worksheets.get_Item(1);
 
-            }
+                    SqlConnection cnn;
+                    string connetionString = (checkBox1.CheckState != CheckState.Checked) ? "Data Source=" + comboBox1.Text + ";User ID=" + textBox1.Text + ";Password=" + textBox2.Text : "Data Source=" + comboBox1.Text + "; Integrated Security=True";
+                    cnn = new SqlConnection(connetionString);
 
+                    if (comboBox2.Text == "CONTPAQ i Nominas"){
+                        try{
+                            cnn.Open();
 
+                            SqlCommand cmd = new SqlCommand("use nomgenerales select NombreEmpresa, RutaEmpresa, GUIDDSL from nom10000 ", cnn);
+                            SqlDataReader dr = cmd.ExecuteReader();
+                            xlWorkSheet.Cells[1, 1] = "NombreEmpresa";
+                            xlWorkSheet.Cells[1, 2] = "NombreBaseDatos";
+                            xlWorkSheet.Cells[1, 3] = "ID ADD BasesDatos";
 
-            Excel.Workbook xlWorkBook;
-            Excel.Worksheet xlWorkSheet;
-            object misValue = System.Reflection.Missing.Value;
+                            int cont = 2;
+                            while (dr.Read()){
+                                xlWorkSheet.Cells[cont, 1] = dr[0].ToString();
+                                xlWorkSheet.Cells[cont, 2] = dr[1].ToString();
+                                xlWorkSheet.Cells[cont, 3] = dr[2].ToString();
+                                cont++;
+                            }
 
-            xlWorkBook = xlApp.Workbooks.Add(misValue);
-            xlWorkSheet = (Excel.Worksheet)xlWorkBook.Worksheets.get_Item(1);
+                            xlWorkBook.SaveAs(textBox3.Text + @"\ReporteNominasDB.xls", Excel.XlFileFormat.xlWorkbookNormal, misValue, misValue, misValue, misValue, Excel.XlSaveAsAccessMode.xlExclusive, misValue, misValue, misValue, misValue, misValue);
+                            xlWorkBook.Close(true, misValue, misValue);
+                            xlApp.Quit();
+                            MessageBox.Show("Archivo Generado Con Exito");
 
-            SqlConnection cnn;
-            string connetionString = "Data Source=" + comboBox1.Text + ";User ID=" + textBox1.Text + ";Password=" +textBox2.Text;
-            cnn = new SqlConnection(connetionString);
-            if (comboBox2.Text == "CONTPAQ i Nominas")
-            {
-                try
-                {
-                    cnn.Open();
+                            Marshal.ReleaseComObject(xlWorkSheet);
+                            Marshal.ReleaseComObject(xlWorkBook);
+                            Marshal.ReleaseComObject(xlApp);
 
-                    SqlCommand cmd = new SqlCommand("use nomgenerales select NombreEmpresa, RutaEmpresa, GUIDDSL from nom10000 ", cnn);
-                    SqlDataReader dr = cmd.ExecuteReader();
-                    xlWorkSheet.Cells[1, 1] = "NombreEmpresa";
-                    xlWorkSheet.Cells[1, 2] = "NombreBaseDatos";
-                    xlWorkSheet.Cells[1, 3] = "ID ADD BasesDatos";
+                            cnn.Close();
+                        }
+                        catch (Exception ex){
+                            MessageBox.Show("Error: " + ex.Message);
+                        }
+                    }else{
+                        try{
+                            cnn.Open();
 
-                    int cont = 2;
-                    while (dr.Read())
-                    {
-                        xlWorkSheet.Cells[cont, 1] = dr[0].ToString();
-                        xlWorkSheet.Cells[cont, 2] = dr[1].ToString();
-                        xlWorkSheet.Cells[cont, 3] = dr[2].ToString();
-                        cont++;
+                            SqlCommand cmd = new SqlCommand("use generalessql select Nombre,AliasBDD from listaempresas ", cnn);
+                            SqlDataReader dr = cmd.ExecuteReader();
+
+                            xlWorkSheet.Cells[1, 1] = "NombreEmpresa";
+                            xlWorkSheet.Cells[1, 2] = "NombreBaseDatos";
+                            xlWorkSheet.Cells[1, 3] = "ID ADD BasesDatos";
+
+                            int cont = 2;
+
+                            Stack nombresEmpresas = new Stack();
+                            Stack nombresDB = new Stack();
+
+                            while (dr.Read()){
+                                nombresEmpresas.Push(dr[0].ToString());
+                                nombresDB.Push(dr[1].ToString());
+                            }
+                            dr.Close();
+
+                            foreach (string db in nombresDB){
+                                try{
+                                    SqlCommand cmd2 = new SqlCommand("use " + db + " select guiddsl from parametros", cnn);
+                                    SqlDataReader dr2 = cmd2.ExecuteReader();
+                                    dr2.Read();
+                                    xlWorkSheet.Cells[cont, 1] = nombresEmpresas.Pop();
+                                    xlWorkSheet.Cells[cont, 2] = db;
+                                    xlWorkSheet.Cells[cont, 3] = dr2[0].ToString();
+                                    cont++;
+                                    dr2.Close();
+                                }
+                                catch (Exception){
+                                    xlWorkSheet.Cells[cont, 1] = nombresEmpresas.Pop();
+                                    xlWorkSheet.Cells[cont, 2] = db;
+                                    xlWorkSheet.Cells[cont, 3] = "empresa no existe";
+                                    cont++;
+                                }
+                            }
+                            xlWorkBook.SaveAs(textBox3.Text + @"\ReporteContabilidadDB.xls", Excel.XlFileFormat.xlWorkbookNormal, misValue, misValue, misValue, misValue, Excel.XlSaveAsAccessMode.xlExclusive, misValue, misValue, misValue, misValue, misValue);
+                            xlWorkBook.Close(true, misValue, misValue);
+                            xlApp.Quit();
+                            MessageBox.Show("Archivo Generado Con Exito");
+
+                            Marshal.ReleaseComObject(xlWorkSheet);
+                            Marshal.ReleaseComObject(xlWorkBook);
+                            Marshal.ReleaseComObject(xlApp);
+
+                            cnn.Close();
+
+                        }catch (Exception ex){
+                            MessageBox.Show("Error: " + ex.Message);
+                        }
                     }
-
-                    xlWorkBook.SaveAs(textBox3.Text + @"\ReporteNominasDB.xls", Excel.XlFileFormat.xlWorkbookNormal, misValue, misValue, misValue, misValue, Excel.XlSaveAsAccessMode.xlExclusive, misValue, misValue, misValue, misValue, misValue);
-                    xlWorkBook.Close(true, misValue, misValue);
-                    xlApp.Quit();
-                    MessageBox.Show("Archivo Generado Con Exito");
-
-                    Marshal.ReleaseComObject(xlWorkSheet);
-                    Marshal.ReleaseComObject(xlWorkBook);
-                    Marshal.ReleaseComObject(xlApp);
-
-                    cnn.Close();
                 }
-                catch (Exception ex)
-                {
-                    MessageBox.Show("Error: " + ex.Message);
+                else {
+                    MessageBox.Show("El direcctorio " + textBox3.Text + " no existe seleccione un directorio existente.");
                 }
             }else {
-                try{
-
-                    cnn.Open();
-
-                    SqlCommand cmd = new SqlCommand("use generalessql select Nombre,AliasBDD from listaempresas ", cnn);
-                    SqlDataReader dr = cmd.ExecuteReader();
-                    
-                    xlWorkSheet.Cells[1, 1] = "NombreEmpresa";
-                    xlWorkSheet.Cells[1, 2] = "NombreBaseDatos";
-                    xlWorkSheet.Cells[1, 3] = "ID ADD BasesDatos";
-
-                    int cont = 2;
-                   
-                    Stack nombresEmpresas = new Stack();
-                    Stack nombresDB = new Stack();
-
-                    while (dr.Read()){
-                        nombresEmpresas.Push(dr[0].ToString());
-                        nombresDB.Push(dr[1].ToString());
-                    }
-                    dr.Close();
-                     
-                    foreach (string db in nombresDB) {
-                        try {
-                            SqlCommand cmd2 = new SqlCommand("use " + db + " select guiddsl from parametros", cnn);
-                            SqlDataReader dr2 = cmd2.ExecuteReader();
-                            dr2.Read();
-                            xlWorkSheet.Cells[cont, 1] = nombresEmpresas.Pop();
-                            xlWorkSheet.Cells[cont, 2] = db;
-                            xlWorkSheet.Cells[cont, 3] = dr2[0].ToString();
-                            cont++;
-                            dr2.Close();
-                        } catch(Exception){
-                            xlWorkSheet.Cells[cont, 1] = nombresEmpresas.Pop();
-                            xlWorkSheet.Cells[cont, 2] = db;
-                            xlWorkSheet.Cells[cont, 3] = "empresa no existe";
-                            cont++;
-
-                        }
-                        
-                       
-                    }
-                    xlWorkBook.SaveAs(textBox3.Text + @"\ReporteContabilidadDB.xls", Excel.XlFileFormat.xlWorkbookNormal, misValue, misValue, misValue, misValue, Excel.XlSaveAsAccessMode.xlExclusive, misValue, misValue, misValue, misValue, misValue);
-                    xlWorkBook.Close(true, misValue, misValue);
-                    xlApp.Quit();
-                    MessageBox.Show("Archivo Generado Con Exito");
-
-                    Marshal.ReleaseComObject(xlWorkSheet);
-                    Marshal.ReleaseComObject(xlWorkBook);
-                    Marshal.ReleaseComObject(xlApp);
-
-                    cnn.Close();
-
-                }
-                catch (Exception ex){
-                    MessageBox.Show("Error: " + ex.Message);
-                }
-            
-            
-            } 
+                MessageBox.Show("Excel no esta instalado en la maquina, no se puede generar el reporte.");
+            }
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e){
@@ -201,8 +191,6 @@ namespace ConectorBaseDatos{
             }
         }
 
-
-
         private void attachListaBasesDeDatosToolStripMenuItem_Click(object sender, EventArgs e){
             bool isOpen = false;
             foreach (Form f in Application.OpenForms){
@@ -223,8 +211,7 @@ namespace ConectorBaseDatos{
             textBox2.Enabled = (checkBox1.CheckState == CheckState.Checked) ? false : true;
         }
 
-        private void checkBox2_CheckedChanged(object sender, EventArgs e)
-        {
+        private void checkBox2_CheckedChanged(object sender, EventArgs e){
             textBox2.UseSystemPasswordChar = checkBox2.CheckState == CheckState.Checked ? false : true;
         }
     }
